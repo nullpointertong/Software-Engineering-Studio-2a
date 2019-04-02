31e863d74d740a1279ea28984ad30ed7e9e05b67
nmoiso <nicholas.moiso@gmail.com>
2019-04-02 21:49:24 +1100

    Helps booking admin

---
+++ app/helps_booking_admin/helps_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HelpsAdminConfig(AppConfig):
+    name = 'helps_admin'
---
+++ app/helps_booking_admin/helps_booking_admin/urls.py
@@ -0,0 +1,35 @@
+"""helps_booking_admin URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from helps_admin import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/' include(django.contrib.auth.urls)),
+    path('', views.sessions, name='sessions'),
+    path('sessions/', views.sessions, name='sessions'),
+    path('workshops/', views.workshops, name='workshops'),
+    path('advisors/', views.advisors, name='advisors'),
+    path('students/', views.students, name='students'),
+    path('waiting_list/' views.waiting_list, name='waiting list'),
+    path('reports/', views.reports, name='reports'),
+    path('template/', views.template, name='template'),
+    path('email/', views.email, name='email'),
+    path('room/', views.room, name='room'),
+    path('messages/', views.messages, name='messages'),
+    path('exit/', views.exit, name='exit')
+]
