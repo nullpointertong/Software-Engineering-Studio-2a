e190e25a14b26484a944a05ab9ab54d63eef5df6
Duc Vu <vutheduc96@gmail.com>
2019-04-03 14:09:50 +1100

    Remove old helps_admin

--- app/helps_booking_system/helps_admin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelpsAdminConfig(AppConfig):
-    name = 'helps_admin'
--- app/helps_booking_system/helps_admin/views.py
+++
@@ -1,7 +0,0 @@
-# Create     your views here.
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-
-# def login_request(request):
-#     context = {'login_request': 'active'}
-#     return render(request, 'registration/login.html', context)
