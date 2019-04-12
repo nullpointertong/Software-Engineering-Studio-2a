ce026cc172f3f2b1a943529e0f9b90306fd5b14d
theducvu <vutheduc96@gmail.com>
2019-04-12 17:39:53 +1000

    Make two sites share one database

--- app/helps_booking_admin/helps_booking_admin/settings.py
+++ app/helps_booking_admin/helps_booking_admin/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'helps_booking_admin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../helps_booking_system/db.sqlite3'),
     }
 }
 
